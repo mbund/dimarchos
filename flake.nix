c331155937abc5dd3f0091025b45f4a00a953b3a
Mark Bundschuh <mark@mbund.dev>
2025-05-01 12:55:30 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    utils,
+    ...
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        clang-ebpf = pkgs.writeShellScriptBin "clang" ''
+        exec ${pkgs.llvmPackages_latest.clang-unwrapped}/bin/clang \
+          -I${pkgs.linuxHeaders}/include \
+          -I${pkgs.libbpf}/include \
+          "$@"
+      '';
+      in rec {
+        devShells.default = pkgs.mkShell {
+          name = "dimarchos";
+          packages = with pkgs; [
+            go
+            gopls
+            go-tools
+            clang-ebpf
+            clang-tools
+            libllvm
+            libbpf
+            linuxHeaders
+          ];
+        };
+      }
+    );
+}
